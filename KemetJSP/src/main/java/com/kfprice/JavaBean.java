ee5bca14a44163f54c8e47342a18f15bda86afce
Kemet Price <90873851+kfprice@users.noreply.github.com>
2022-01-12 09:59:16 -0500

    Add files via upload

---
+++ KemetJSP/src/main/java/com/kfprice/JavaBean.java
@@ -0,0 +1,24 @@
+package com.kfprice;
+
+public class JavaBean {
+
+	private int empId;
+	private String empName;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String string) {
+		this.empId = string;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+}
